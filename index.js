b9453cdface8af844a532e2d040386384ef89ec5
Alvine Mugo <alvnmugo@example.com>
2024-07-11 10:15:04 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
+let num1 = 31;
+let num2 = 2;
+let multiply = num1 * num2;
 
-// Write your code here
+console.log(multiply);
+
+const random = Math.floor(Math.random() * 100) + 1;
+console.log(random);
+
+let num3 = 9;
+let num4 = 5;
+let mod = (num3 % num4)
+console.log(mod);
+
+const numbers = [10, 5,15,20]
+const max = Math.max(...numbers);
+console.log(max);
